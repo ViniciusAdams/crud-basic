22b4ab39fe587c635de5bd82a6173dc61930e7df
ViniciusAdams <viniciusadams98@gmail.com>
2026-07-02 21:01:41 +0100

    commit last changeds

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ engine = create_engine(DATABASE_URL)
 
 @app.get("/")
 def home():
-    return {"message":"API is working"}
+    return {"message": "API is working"}
 
 
 @app.get("/db-test")
@@ -19,7 +19,26 @@ def db_test():
         result = connection.execute(text("SELECT DATABASE();"))
         database_name = result.scalar()
 
-        return {
-            "message": "Database connection is working",
-            "database": database_name
-        }
+    return {
+        "message": "Database connection is working",
+        "database": database_name
+    }
+
+
+# By opening GET /users, the function below will be run
+@app.get("/users")
+def get_users():
+    # Opens the database connection
+    with engine.connect() as connection:
+        # Runs normal SQL
+        result = connection.execute(
+            text("SELECT id, name, email, created_at FROM users;")
+        )
+
+        users = []
+
+        # Turns MariaDB rows into Python dictionaries
+        for row in result.mappings():
+            users.append(dict(row))
+
+    return users
